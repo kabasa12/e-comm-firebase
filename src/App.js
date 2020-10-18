413dfa947a012c151a9090220f13b7dccacb4aea
kabasa12 <kabasa12@gmail.com>
2020-10-18 13:49:36 +0300

    add redux to handle state

--- src/App.js
+++ src/App.js
@@ -1,37 +1,32 @@
 import React from 'react';
 import './App.css';
 import {Switch,Route} from 'react-router-dom';
-
+import {connect} from 'react-redux';
 import HomePage from './pages/homePage/homepage.component';
 import ShopPage from './pages/shopPage/shop.component';
 import Header from './components/header/header.component';
 import AuthPage from './pages/auth/auth.component';
 import {auth,createUserProfileDocument} from './firebase/firebase.util';
+import { setCurrentUser} from './redux/user/user.action';
 
 class App extends React.Component {
-  constructor(){
-    super();
-    this.state = {
-      currentUser:null
-    }
-  }
 
   unSubscribeFromAuth = null;
   componentDidMount(){
+    const {setCurrentUser} = this.props;
+
     this.unSubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if(userAuth){
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser:{
-              id:snapShot.id,
-              ...snapShot.data()
-            }
+          setCurrentUser({
+            id:snapShot.id,
+            ...snapShot.data()
           });
         });
       } else {
-        this.setState({currentUser:userAuth})
+        setCurrentUser(userAuth)
       }
     });
   }
@@ -43,7 +38,7 @@ class App extends React.Component {
   render(){
     return (
       <div>
-        <Header currentUser={this.state.currentUser}/>
+        <Header />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
@@ -55,4 +50,7 @@ class App extends React.Component {
   
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser : user => dispatch(setCurrentUser(user))
+})
+export default connect(null,mapDispatchToProps)(App);
--- src/components/sigin-in/sign-in.component.js
+++ src/components/sigin-in/sign-in.component.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import {signInWithGoogle} from '../../firebase/firebase.util'
+import {auth,signInWithGoogle} from '../../firebase/firebase.util'
 
 import './sign-in.styles.scss';
 
@@ -20,11 +20,19 @@ class SignIn extends React.Component{
         this.setState({[name]:value})
     }
 
-    handleSubmit = (e) => {
+    handleSubmit = async (e) => {
         e.preventDefault();
+        const {email,password} = this.state;
 
-        this.setState({email:"",password:""})
+        try{
+            await auth.signInWithEmailAndPassword(email,password);
+            this.setState({email:"",password:""});
+        }catch(err){
+            console.log(err)
+        }
     }
+
+    
     render() {
         return(
             <div className="sign-in">
